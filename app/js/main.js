7dbb73092c22048c51c5e2c765b98f8548afc2a1
AlexIstra1989 <alex888fcsm@gmail.com>
2022-02-20 23:37:40 +0300

    added catalog page

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,38 @@
 $(document).ready(function(){
+ $('.menu__burger').on('click', function() {
+  $('.menu').toggleClass('menu--active'),
+  $('.menu__burger').toggleClass('menu__burger--active');
+ });
+
+ $('.menu__burger').click(function (event) {
+  event.preventDefault();
+  $('body').toggleClass('overflow');
+ });
+
  $('.slider__inner').slick({
   dots: true,
-  arrows: true,
+  arrows: false,
   infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1
  });
+
+ $('.catalog__price-scale').ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 999,
+  prefix: $,
+  onStart: function (data) {
+   $('.catalog__price-from').text(data.from);
+   $('.catalog__price-to').text(data.to);
+  },
+  onChange: function (data) {
+   $('.catalog__price-from').text(data.from);
+   $('.catalog__price-to').text(data.to);
+  },
+ });
+
+
 });
+
+
